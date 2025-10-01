24d3cdd72e182a867083828afdb2435fe9be7577
Oyu Chinbat <c.oyu@wustl.edu>
2025-10-01 13:54:51 -0500

    Completed

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -5,7 +5,25 @@ import java.awt.Color;
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
-	public static void main(String[] args) {
-		
-	}
+   public static void main(String[] args) {
+      norway();
+   }
+
+   private static void mother() {
+      StdDraw.clear();
+      StdDraw.setPenColor(150, 0, 100);
+      StdDraw.filledRectangle(3, 3, 3, 3);
+      StdDraw.filledRectangle(3, 13, 3, 3);
+      StdDraw.filledRectangle(16, 3, 6, 3);
+      StdDraw.filledRectangle(16, 13, 6, 3);
+      StdDraw.setPenColor(0, 170, 127);
+      StdDraw.filledRectangle(11, 8, 11, 1);
+      StdDraw.filledRectangle(8, 8, 1, 8);
+   }
+
+   public static void norway() {
+      StdDraw.setXscale(0, 22);
+      StdDraw.setYscale(0, 16);
+      mother();
+   }
 }
